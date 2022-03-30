3879e948d4ec3cc5f3911a3d46bb425a36986254
Mark-Gallagher <92432812+Mark-Gallagher@users.noreply.github.com>
2022-03-30 10:04:43 +0100

    Add files via upload

---
+++ src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -0,0 +1,19 @@
+package ie.gmit.exercise;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainMessageApp {
+	public static void main(String[] args) throws IOException {
+
+		Formatter formatter = new TextFormatter();//creates formatter
+
+		PrintWriter writer = new PrintWriter(System.out);
+
+		Message msg = new Message("This is a message");
+		MessagePrinter printer = new MessagePrinter();
+		printer.writeMessage(formatter,writer,msg);
+	}
+
+
+}
---
+++ src/main/java/ie/gmit/exercise/MessagePrinter.java
@@ -0,0 +1,14 @@
+package ie.gmit.exercise;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MessagePrinter extends MainMessageApp {
+
+		//Writes message to a file
+		public void writeMessage(Formatter formatter,PrintWriter writer,Message msg) throws IOException {
+			writer.println(formatter.format(msg)); //formats and writes message
+			writer.flush();
+			writer.close();
+		}
+}
